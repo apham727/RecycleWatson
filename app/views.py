3bd48d298e66fdd47cd426bb809b110231e64172
Andrew Pham <apham727@gmail.com>
2019-06-04 22:56:35 -0500

    functioning park search with first css implementation

--- app/views.py
+++ app/views.py
@@ -1,50 +1,107 @@
 from flask import Flask, render_template, request, flash, redirect
 from app import app
-from data import db_articles
-import shoebox
-
 import os
+import urllib.request, json
+
+
 
 temp = ["ndb"]
-articles = db_articles(temp)
 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+endpoint = "https://developer.nps.gov/api/v1/parks?api_key=1dPktcL1TRUd1cJLwxBWAgJuaVwmmwbvTMfe41rV"
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
+
 @app.route('/')
 @app.route('/index')
 def index():
+    # # Configure API request
+    # endpoint = "https://developer.nps.gov/api/v1/parks?api_key=1dPktcL1TRUd1cJLwxBWAgJuaVwmmwbvTMfe41rV"
+    # HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
+    # req = urllib.request.Request(endpoint, headers=HEADERS)
+    # # Execute request and parse response
+    # response = urllib.request.urlopen(req).read()
+    # data = json.loads(response.decode('utf-8'))
+    #
+    # # Prepare and execute output
+    # for park in data["data"]:
+    #     print(park["fullName"])
+    print("DONE")
     return render_template('index.html')
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    url = request.form['url']
+@app.route('/search_result', methods=['POST'])
+def search_result():
+    ### Query the API for a park and take the first response
+    query = request.form['query']
+    endpoint = "https://developer.nps.gov/api/v1/parks?api_key=1dPktcL1TRUd1cJLwxBWAgJuaVwmmwbvTMfe41rV"
+    url = endpoint + "&q=" + query + "&fields=images"
+    req = urllib.request.Request(url, headers=HEADERS)
+    response = urllib.request.urlopen(req).read()
+    json_resp = json.loads(response.decode('utf-8'))
 
-    if len(url) > 0:
-        print(url)
-        temp = shoebox.classify_image_url(url)
-        print(temp)
-    else:
-        target = os.path.join(APP_ROOT, 'images/')
-        print(target)
+    # If the list is empty, the response returned nothing
+    if not json_resp["data"]:
+        return "Nothing found"
 
-        if not os.path.isdir(target):
-            os.mkdir(target)
+    # Otherwise, we take the top result
+    top_result = json_resp["data"][0]
+    park_code = top_result["parkCode"]
+    # print(len(top_result))
+    # print(top_result)
+    # print(top_result["fullName"])
+    # print(top_result["description"])
 
-        for file in request.files.getlist("file"):
-            print(file)
-            filename = file.filename
-            print(filename)
-            destination = "/".join([target, filename])
-            print(destination)
-            file.save(destination)
-            temp = shoebox.classify_local_image(url)
-            print(temp)
+    #### Grab relevant information about the specific location #####
+    ##### Alerts #####
+    alerts = get_alerts(park_code)
+    ### News Articles #####
+    articles = get_new_articles(park_code)
+    print("num news articles is " + str(len(articles)))
 
-        url = destination
-        print(url)
+    # print(top_result)
 
-    return render_template("result.html", url=url, articles=db_articles(temp))
+
+    # In case the api doesn't return an image on the query
+    try:
+        image_url = top_result["images"][0]['url']
+    except:
+        image_url = ""
+
+    return render_template('search_result.html',
+                           name = top_result["fullName"],
+                           description = top_result["description"],
+                           image_url = image_url,
+                           news_articles = articles, alerts = alerts)
+
+
+
+def get_alerts(parkcode):
+    alert_endpoint = "https://developer.nps.gov/api/v1/alerts?api_key=1dPktcL1TRUd1cJLwxBWAgJuaVwmmwbvTMfe41rV"
+    url = alert_endpoint + "&parkCode=" + str(parkcode)
+    req = urllib.request.Request(url, headers=HEADERS)
+    response = urllib.request.urlopen(req).read()
+    json_resp = json.loads(response.decode('utf-8'))
+
+    # Extracts the top three alerts
+    alerts = json_resp["data"]
+    if len(json_resp["data"]) > 4:
+        alerts = json_resp["data"][0:3]
+
+    return alerts
+
+def get_new_articles(parkcode):
+    alert_endpoint = "https://developer.nps.gov/api/v1/articles?api_key=1dPktcL1TRUd1cJLwxBWAgJuaVwmmwbvTMfe41rV"
+    url = alert_endpoint + "&parkCode=" + str(parkcode)
+    req = urllib.request.Request(url, headers=HEADERS)
+    article_response = urllib.request.urlopen(req).read()
+    article_json_resp = json.loads(article_response.decode('utf-8'))
+
+    # Extracts the top three news articles
+    articles = article_json_resp["data"]
+    if len(article_json_resp["data"]) > 4:
+        articles = article_json_resp["data"][0:3]
+    return articles
 
